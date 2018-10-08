bc52c06b724db59a77344bdedb61a33ee20ca89e
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2018-10-08 18:06:42 +0200

    added files

---
+++ arch-install.sh
@@ -0,0 +1,21 @@
+info() {
+    printf "$*\n"
+}
+
+wait_to_continue() {
+    info "Press any key to continue"
+    read -n 1
+    info "\n\n\n"
+}
+
+STAGE=${1:-pre}
+
+cd $(dirname $(realpath $0))
+
+OLD_PWD=${PWD}
+cd instructions/${STAGE}
+for instruction in `ls`; do
+    bash ./$instruction || wait_to_continue
+done
+cd $OLD_PWD
+exit 0

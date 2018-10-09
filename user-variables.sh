7fc026156be0d72c4db6ebad51af284eb25f5e8e
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2018-10-09 11:06:28 +0200

    moved variables to seperate file

---
+++ user-variables.sh
@@ -0,0 +1,18 @@
+# vim: ft=sh
+
+export PACKAGES=(
+    base
+    base-devel
+
+    openssh
+    python3
+)
+
+export TGTDEV=/dev/nvme0n1
+export ROOT_PARTITION_SIZE=64
+
+export HOSTNAME=laptop
+export DOMAIN=local
+
+export KEYMAP=no-latin1
+

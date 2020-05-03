5daf853c4f4299fa04a316e4704df53859e37498
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2020-05-03 16:50:15 +0200

    added workstation

---
+++ host-variables/workstation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export TIMEZONE=Europe/Oslo
+
+export TGTDEV=/dev/nvme0n1
+export ROOT_PARTITION_SIZE=128
+#export SWAP_SIZE=              # Leave uncommented to calculate from total memory
+
+export HOSTNAME=workstation
+export DOMAIN=local
+
+export KEYMAP=no-latin1
+export EFI=true

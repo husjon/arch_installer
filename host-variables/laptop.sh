f8d27df94be202f96afd5038eaba9db6e1d691e1
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2020-05-03 16:50:41 +0200

    Update host variables

--- host-variables/laptop.sh
+++ host-variables/laptop.sh
@@ -2,11 +2,11 @@
 
 export TIMEZONE=Europe/Oslo
 
-export TGTDEV=
-export ROOT_PARTITION_SIZE=
+export TGTDEV=/dev/nvme0n1
+export ROOT_PARTITION_SIZE=64
 #export SWAP_SIZE=              # Leave uncommented to calculate from total memory
 
-export HOSTNAME=
+export HOSTNAME=laptop
 export DOMAIN=local
 
 export KEYMAP=no-latin1
